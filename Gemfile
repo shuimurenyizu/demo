4c0886c22beddd557b213cb20258865bfba4b7f0
shuimurenyizu <shuimurenyizu@163.com>
2013-08-03 05:53:53 +0800

    first commit

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+ruby '1.9.3'
+
+gem 'rails', '3.2.13'
+gem 'jquery-rails', '2.2.1'
+gem 'jbuilder', '1.0.2'
+gem 'turbolinks', '1.1.1'
+
+group :development do
+	gem 'sqlite3', '1.3.7'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :doc do
+  gem 'sdoc', '0.3.20', require: false
+end
+
+# 使用 PostgreSQL
+group :production do
+  gem 'pg', '0.15.1'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '23fcdd1eab217ee3e7e0e9f6dfef82eb245847655057f7c8ce38adcfb7d8c17694f97acfbd950f947d08ca199c4733695b7f0a79d9e55b5c0b282ae72446e5b1'
